68d543550e2611e0a8b974e0cc7cfb893ea584e8
Frank <akura11.tt@gmail.com>
2017-04-11 17:56:36 -0500

    Ajustes de parametrizacion de arvhios estaticos

--- Buda/Buda/settings.py
+++ Buda/Buda/settings.py
@@ -138,7 +138,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
 
-STATIC_URL = '/static/buda/'
+
+STATIC_URL = os.environ.get('CDN_STATIC_FILES_URL', '/static/buda/')
 
 URL_BUDA_API = os.environ.get('URL_BUDA_API')
 CORS_ORIGIN_ALLOW_ALL = True
